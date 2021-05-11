10fd147ff1d6ccc5fcfaa6c8b8cd3cfaa893856b
LS-io <luka.smajila@gmail.com>
2021-05-11 23:27:04 +0200

    "Continued the Numpy module"

--- chapter2_pythonforstatistics/numpy_basics/np_arrays.py
+++ chapter2_pythonforstatistics/numpy_basics/np_arrays.py
@@ -18,3 +18,34 @@ b = np.array([1, 2, 'a'])
 c = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 #print(c)
 # We often refer to multidimensional arrays as matrices
+
+
+## We can also use methods np.zeros() and np.ones() to create a matrix full of zeros or ones
+zero_array = np.zeros((2, 2)) # This creates a 2 by 2 matrix
+#print(zero_array)
+
+ones_array = np.ones((2, 3, 5), dtype = int) # Creates a 3D integer matrix
+#print(ones_array)
+
+
+## Similarly to zeros and ones, we can generate a matrix filled with random numbers
+random_array = np.random.rand(2, 3, 4)
+#print(random_array)
+# We see that this generate a matric with random float values between 0 (inclusive) and 1 (exclusive)
+# If we want to generate a matrix of random integers, we can use the np.random.randint() method
+randomint_array = np.random.randint(0, 10, size = (2, 3, 4))
+#print(randomint_array)
+
+########################################################################################################
+## Similar method I like to use, that generate a matrix with random values from a specific distribution:
+## * np.random.normal --> returns random values from a normal (Gauss) distribution
+## * np.random.pareto --> returns random values from a pareto distribution
+## * np.random.power --> returns random values from a power distribution
+########################################################################################################
+
+
+## Keep in mind, that when calling multidimensional arrays, we simply need to separate the individual indices with commas
+## Although calling with separated square brackets also works
+
+
+##
