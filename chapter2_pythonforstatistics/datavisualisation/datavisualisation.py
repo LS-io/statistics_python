a5bb1f5cd1dbea4591da5cb4bb7ce959e02842c3
LS-io <luka.smajila@gmail.com>
2021-05-26 17:20:15 +0200

    continued with scatterplot

--- chapter2_pythonforstatistics/datavisualisation/datavisualisation.py
+++ chapter2_pythonforstatistics/datavisualisation/datavisualisation.py
@@ -5,3 +5,23 @@
 ## The main workhorse of visualisation functionality in Python is the pyplot package.
 import matplotlib.pyplot as plt
 
+#####################################################################################
+##
+## Scatter plots
+##
+## The scatter plot is one of the most fundamental visualisation methods.
+## It plots a list of points on a plane (or any other higer dimensional space).
+## This is done with the plt.scatter() function, as we will test now
+x = [1, 2, 3, 1.5, 2]
+y = [-1, 5, 2, 3, 0]
+
+#plt.scatter(x, y)
+#plt.show()
+
+## We can specify additional arguments in the plt.scatter() function
+## e.g. specify the individual points, and colors
+sizes = [10, 40, 60, 80, 100]
+colors = ['r', 'b', 'y', 'g', 'k']
+
+plt.scatter(x, y, s = sizes, c = colors)
+plt.show()
