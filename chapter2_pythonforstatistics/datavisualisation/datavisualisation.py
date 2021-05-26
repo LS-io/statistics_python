733cc7c0c668ef91fb8e34f27c4d4d6323cdaff2
LS-io <luka.smajila@gmail.com>
2021-05-26 15:08:59 +0200

    started with the visualisation part

--- chapter2_pythonforstatistics/datavisualisation/datavisualisation.py
+++ chapter2_pythonforstatistics/datavisualisation/datavisualisation.py
@@ -1,1 +1,7 @@
-## start on page 92 in the book
+## We will start with installing the required packages for our visualisation
+# !pip install matplotlib seaborn --update --quiet
+
+## Next we need to import the package for us to use it.
+## The main workhorse of visualisation functionality in Python is the pyplot package.
+import matplotlib.pyplot as plt
+
