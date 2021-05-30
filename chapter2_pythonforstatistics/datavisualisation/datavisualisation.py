d58be253cf983a96252f72f9ef658e037685283e
LS-io <luka.smajila@gmail.com>
2021-05-30 19:23:24 +0200

    did line graphs

--- chapter2_pythonforstatistics/datavisualisation/datavisualisation.py
+++ chapter2_pythonforstatistics/datavisualisation/datavisualisation.py
@@ -23,5 +23,31 @@ y = [-1, 5, 2, 3, 0]
 sizes = [10, 40, 60, 80, 100]
 colors = ['r', 'b', 'y', 'g', 'k']
 
-plt.scatter(x, y, s = sizes, c = colors)
+#plt.scatter(x, y, s = sizes, c = colors)
+#plt.show()
+
+
+## Line Graphs
+## Now let's take a look at plotting graphs with points represented along a curve
+import numpy as np
+
+x = np.linspace(0, 10, 1000)
+y = np.sin(x)
+
+#plt.plot(x, y)
+#plt.show()
+
+## Similarly as with the scatter plot, we can customise various elements on our line graphs
+x = np.linspace(1, 10, 1000)
+linear_line = x
+log_curve = np.log(x)
+sin_wave = np.sin(x)
+
+curves = [linear_line, log_curve, sin_wave]
+colors = ['k', 'r', 'b']
+styles = ['-', '--', ':']
+
+for curve, color, style in zip(curves, colors, styles):
+    plt.plot(x, curve, c = color, linestyle = style)
+
 plt.show()
